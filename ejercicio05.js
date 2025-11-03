c0222532b47ffff5cf891c834870edbc2e7304e4
Alejandro <amendozapan@unsa.edu.pe>
2025-11-03 16:21:25 -0500

    Ejercicio 05

---
+++ ejercicio05.js
@@ -0,0 +1,5 @@
+let dado1 = Math.round(Math.random() * 6) + 1;
+let dado2 = Math.round(Math.random() * 6) + 1;
+console.log("Dado 1: "+dado1);
+console.log("Dado 2: "+dado2);
+console.log("La suma es "+(dado1+dado2));
