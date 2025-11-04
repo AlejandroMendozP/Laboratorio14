b81f5b318175c024d1b387d4b1f04409976bd05a
Alejandro <amendozapan@unsa.edu.pe>
2025-11-04 14:59:02 -0500

    Ejercicio 16

--- ejercicio16.js
+++ ejercicio16.js
@@ -28,3 +28,13 @@ class ProgramadorSenior extends Programador {
         return super.calcularSueldo() / 1.10 * 1.25; 
   }
 }
+
+const empleado = new Empleado("Carlos", 2000);
+const programador = new Programador("Ana", 2500, "JavaScript");
+const senior = new ProgramadorSenior("Luis", 3000, "Python");
+
+console.log("Nombre: " + empleado.nombre + "\nSueldo: " + empleado.calcularSueldo());
+console.log("Nombre: " + programador.nombre + "\nLenguaje: " + programador.lenguaje);
+console.log("Sueldo: " + programador.calcularSueldo());
+console.log("Nombre: " + senior.nombre + "\nLenguaje: " + senior.lenguaje);
+console.log("Sueldo: " + senior.calcularSueldo());
